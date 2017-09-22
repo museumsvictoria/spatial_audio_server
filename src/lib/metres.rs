1cb0fce3e6286ec21562fb0237c65ab956d96493
mitchmindtree <mitchell.nordine@gmail.com>
2017-09-22 18:02:37 +1000

    Finish implementation of speaker editor GUI
    This adds the ability to assign a unique channel per speaker and to drag
    speakers to positions (in metres) within the space in a thread-safe
    manner so that positions may be used for the distance to amplitude
    translation function on the audio thread.
    
    Closes #5

--- src/lib/metres.rs
+++ src/lib/metres.rs
@@ -2,12 +2,14 @@ custom_derive! {
     /// Used for describing locations throughout the installation.
     ///
     /// Using this GUI agnostic measurement greatly simplifies positioning and makes it easier
-    #[derive(Copy, Clone, Debug, Default, PartialEq, PartialOrd, NewtypeFrom,
+    #[derive(Copy, Clone, Debug, Default, PartialEq, PartialOrd,
+             NewtypeFrom,
              NewtypeAdd, NewtypeSub, NewtypeMul, NewtypeMul(f64), NewtypeDiv, NewtypeDiv(f64),
              NewtypeAddAssign, NewtypeSubAssign, NewtypeMulAssign, NewtypeDivAssign,
              NewtypeMulAssign(f64), NewtypeDivAssign(f64),
              NewtypeRem, NewtypeRemAssign,
              NewtypeNeg)]
+    #[derive(Serialize, Deserialize)]
     pub struct Metres(pub f64);
 }
 
