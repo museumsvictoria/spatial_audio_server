c47ab3192f5511c73b374dc94d3fca063d649ada
mitchmindtree <mitchell.nordine@gmail.com>
2018-01-31 15:43:17 +1100

    Integrate FFT analysis for all output channels and include data in OSC
    This commit integrates the RustFFT crate for performing a pure rust fast
    fourier transform for audio analysis. As the RustFFT crate is not
    audio-specific, this adds a new `fft` module for mapping audio samples
    to complex numbers for the FFT input and for decoding the output complex
    numbers into frequency amplitudes for each bin. It also provides
    functions for mapping peaks from linearly divided frequency bins to bins
    divided via Mel-frequencies. This means the resulting frequency bins
    should better match human perception of frequency differences.
    
    Each computer listed for each installation will now receive the average
    peak, RMS, 3-band (low-mid-high) and 8-band fourier transform for plugging into
    visualisations. They also continue to receive the peak and RMS
    per-speaker in the installation. This should complete the OSC output
    data story for the exhibition.
    
    Closes #12.
    Closes #13.

--- src/lib/audio/detector.rs
+++ src/lib/audio/detector.rs
@@ -4,6 +4,7 @@
 
 use audio;
 use nannou::audio::sample::{self, ring_buffer};
+use rustfft::num_complex::Complex;
 
 // The frame type used within the detectors.
 type FrameType = [f32; 1];
@@ -20,6 +21,9 @@ type RmsDetector = sample::envelope::Detector<FrameType, Rms>;
 // The Peak envelope detector used for monitoring the signal.
 type PeakDetector = sample::envelope::Detector<FrameType, Peak>;
 
+// The type of FFT used for analysis.
+pub type Fft = super::fft::Fft<[Complex<f32>; FFT_WINDOW_LEN]>;
+
 // RMS is monitored for visualisation, so we want a window size roughly the duration of one frame.
 //
 // A new visual frame is displayed roughly 60 times per second compared to 44_100 audio frames.
@@ -31,18 +35,29 @@ const RMS_RELEASE_FRAMES: f32 = 0.0;
 const PEAK_ATTACK_FRAMES: f32 = WINDOW_SIZE as f32 / 8.0;
 const PEAK_RELEASE_FRAMES: f32 = WINDOW_SIZE as f32 / 8.0;
 
+/// The length of the window used for performing the FFT.
+pub const FFT_WINDOW_LEN: usize = 1024;
+
+/// The step between each frequency bin is equal to `samplerate / 2 * windowlength`.
+pub const FFT_BIN_STEP_HZ: f64 = audio::SAMPLE_RATE / (2.0 * FFT_WINDOW_LEN as f64);
+
 /// An envelope detector for a single channel.
 ///
 /// Detects RMS and Peak envelopes.
-pub struct Detector {
+pub struct EnvDetector {
     rms: RmsDetector,
     peak: PeakDetector,
     current_rms: f32,
     current_peak: f32,
 }
 
-impl Detector {
-    /// Construct a new `Detector` with a zeroed RMS window.
+/// An FFT frequency amplitude detector for a single channel.
+pub struct FftDetector {
+    fft_samples: ring_buffer::Fixed<[f32; FFT_WINDOW_LEN]>,
+}
+
+impl EnvDetector {
+    /// Construct a new `EnvDetector` with a zeroed RMS window.
     pub fn new() -> Self {
         let slice = vec![[0.0]; WINDOW_SIZE].into_boxed_slice();
         let ring_buffer = ring_buffer::Fixed::from(slice);
@@ -50,7 +65,12 @@ impl Detector {
         let peak = PeakDetector::peak(PEAK_ATTACK_FRAMES, PEAK_RELEASE_FRAMES);
         let current_rms = 0.0;
         let current_peak = 0.0;
-        Detector { rms, peak, current_rms, current_peak }
+        EnvDetector {
+            rms,
+            peak,
+            current_rms,
+            current_peak,
+        }
     }
 
     /// Step forward the detector with the given sample.
@@ -67,5 +87,27 @@ impl Detector {
     /// Returns the current RMS and peak.
     pub fn current(&self) -> (f32, f32) {
         (self.current_rms, self.current_peak)
+    }
+}
+
+impl FftDetector {
+    /// Construct a new `EnvDetector` with a zeroed RMS window.
+    pub fn new() -> Self {
+        let fft_samples = ring_buffer::Fixed::from([0.0; FFT_WINDOW_LEN]);
+        FftDetector {
+            fft_samples,
+        }
+    }
+
+    /// Insert a new sample into the ring buffer.
+    ///
+    /// Returns the current RMS and peak.
+    pub fn push(&mut self, sample: f32) {
+        self.fft_samples.push(sample);
+    }
+
+    /// Calculate the FFT for all samples currently in the current ring buffer.
+    pub fn calc_fft(&self, fft: &mut Fft, freq_amps: &mut [f32]) {
+        fft.process(self.fft_samples.iter().cloned(), freq_amps);
     }
 }
